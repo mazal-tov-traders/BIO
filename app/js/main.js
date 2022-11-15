566d39019f355064604186b0068a176c190590af
mazal-tov-traders <d90.german@gmail.com>
2022-11-15 15:28:25 +0200

    added about

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,17 @@
-$(function(){
+
+$(function () {
 
 
-  
+  $('.slider__wrapper').slick({
+    dots: true,
+    infinite: true,
+    speed: 1200,
+    // cssEase: 'linear',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    
+  });
 
-  
 
 });
+
