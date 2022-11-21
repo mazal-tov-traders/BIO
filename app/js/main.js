45572462530df423aef26eb667faeff376ed9f91
mazal-tov-traders <d90.german@gmail.com>
2022-11-21 14:37:27 +0200

    all

--- app/js/main.js
+++ app/js/main.js
@@ -6,11 +6,22 @@ $(function () {
     dots: true,
     infinite: true,
     speed: 1200,
-    // cssEase: 'linear',
+    
     slidesToShow: 1,
     slidesToScroll: 1,
     
   });
+
+  $('.slider__wrapper-price').slick({
+    dots: true,
+    infinite: true,
+    speed: 1200,
+    
+    slidesToShow: 1,
+    slidesToScroll: 1,
+
+  });
+
 
 
 });
